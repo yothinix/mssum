b0064fe35a7cab6f20d6d68c0baf9c4da6aee87f
Yothin M <m@yothinix.com>
2018-07-21 22:53:53 +0700

    Make request to github, ensure HTTPoison start everytime :rocket:

--- lib/mix/tasks/main.ex
+++ lib/mix/tasks/main.ex
@@ -3,6 +3,7 @@ defmodule Mix.Tasks.Main do
 
   @shortdoc "Simple main runner"
   def run(_) do
+    {:ok, _started} = Application.ensure_all_started(:httpoison)
     Mssum.main()
   end
 end
--- lib/mssum.ex
+++ lib/mssum.ex
@@ -1,11 +1,13 @@
 defmodule Mssum do
   def main do
-    issue = %{
-      :id => 1,
-      :name => "Some issue name"
-    }
-
-    IO.puts issue[:name]
+    case HTTPoison.get("https://api.github.com") do
+      {:ok, %HTTPoison.Response{status_code: 200, body: body}} ->
+        IO.puts body
+      {:ok, %HTTPoison.Response{status_code: 404}} ->
+        IO.puts "Not found :("
+      {:error, %HTTPoison.Error{reason: reason}} ->
+        IO.inspect reason
+    end
 
   end
 end
