92b72b33dac8e8c37181ee483f538a080db53b12
Yothin M <m@yothinix.com>
2018-07-21 23:03:59 +0700

    Install poison :rocket:
    - JSON parser

--- mix.exs
+++ mix.exs
@@ -21,7 +21,8 @@ defmodule Mssum.MixProject do
   # Run "mix help deps" to learn about dependencies.
   defp deps do
     [
-      {:httpoison, "~>1.0"}
+      {:httpoison, "~>1.0"},
+      {:poison, "~> 3.1"}
     ]
   end
 end
