524816ca20ad725caaae28f8387d46f0ff0658c8
Yothin M <m@yothinix.com>
2018-07-21 23:16:33 +0700

    Decode json body to be map :rocket:

--- lib/mssum.ex
+++ lib/mssum.ex
@@ -1,13 +1,16 @@
 defmodule Mssum do
   def main do
+    body = make_request()
+  end
+
+  def make_request do
     case HTTPoison.get("https://api.github.com") do
       {:ok, %HTTPoison.Response{status_code: 200, body: body}} ->
-        IO.puts body
+        body |> Poison.decode!
       {:ok, %HTTPoison.Response{status_code: 404}} ->
         IO.puts "Not found :("
       {:error, %HTTPoison.Error{reason: reason}} ->
         IO.inspect reason
     end
-
   end
 end
