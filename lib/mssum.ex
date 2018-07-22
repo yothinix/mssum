3eeb748dc5471162686b36b55f5b1ac16c83ca4e
Yothin M <m@yothinix.com>
2018-07-22 10:53:23 +0700

    Add endpoint argument and its default value :rocket:

--- lib/mssum.ex
+++ lib/mssum.ex
@@ -1,11 +1,14 @@
 defmodule Mssum do
   def main do
-    body = make_request()
+    make_request()
   end
 
-  def make_request do
-    case HTTPoison.get("https://api.github.com") do
+  def make_request(endpoint \\ "/") do
+    base_url = "https://api.github.com#{endpoint}"
+
+    case HTTPoison.get(base_url) do
       {:ok, %HTTPoison.Response{status_code: 200, body: body}} ->
+        IO.puts body
         body |> Poison.decode!
       {:ok, %HTTPoison.Response{status_code: 404}} ->
         IO.puts "Not found :("
