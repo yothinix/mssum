aa3175829ccbf2ea07c8ac2db23645b7f1a2ec5d
Yothin M <m@yothinix.com>
2018-07-22 20:06:40 +0700

    Re-format code :rocket:

--- lib/mssum.ex
+++ lib/mssum.ex
@@ -1,24 +1,23 @@
 defmodule Mssum do
   @github_base_url Application.get_env(:app_vars, :githubBaseUrl)
+  @milestone_field ~w(
+    closed_issues created_at description due_on html_url number state title
+  )
+  @issue_field ~w(
+    assignees body closed at created_at html_url labels number state title
+  )
 
   def main do
-    user = "yothinix"
-    repository = "mssum"
+    milestone =
+      get_latest_milestone("yothinix", "mssum")
+      |> Map.take(@milestone_field)
 
-    select_milestone_fields = ~w(
-      closed_issues created_at description due_on html_url number state title
-    )
-    milestone = get_latest_milestone(user, repository)
-    |> Map.take(select_milestone_fields)
+    select_issue_fields = fn issue -> Map.take(issue, @issue_field) end
 
-    fields = ~w(
-      assignees body closed at created_at html_url labels number state title
-    )
-    select_issue_fields = fn issue -> Map.take(issue, fields) end
-
-    issues = milestone
-    |> get_all_issues(user, repository)
-    |> Enum.map(select_issue_fields)
+    issues =
+      milestone
+      |> get_all_issues("yothinix", "mssum")
+      |> Enum.map(select_issue_fields)
 
     render_email_template(milestone, issues)
   end
@@ -33,7 +32,7 @@ defmodule Mssum do
     Sprint End: #{milestone["due_on"]}
     Milestone page: #{milestone["html_url"]}
     """
-    |> IO.puts
+    |> IO.puts()
 
     render = fn issue ->
       """
@@ -42,26 +41,26 @@ defmodule Mssum do
       """
     end
 
-    issues |> Enum.map(render) |> IO.puts
+    issues |> Enum.map(render) |> IO.puts()
   end
 
   def get_all_issues(milestone, user, repo) do
     params = %{
-      "milestone": milestone["number"],
-      "state": "all"
+      milestone: milestone["number"],
+      state: "all"
     }
-    make_request("/repos/#{user}/#{repo}/issues", params)
-    |> Poison.decode!
+
+    make_request("/repos/#{user}/#{repo}/issues", params) |> Poison.decode!()
   end
 
   def get_latest_milestone(user, repo) do
     make_request("/repos/#{user}/#{repo}/milestones")
-    |> Poison.decode!
+    |> Poison.decode!()
     |> Enum.at(-1)
     |> Map.get("url")
     |> String.replace(@github_base_url, "")
     |> make_request
-    |> Poison.decode!
+    |> Poison.decode!()
   end
 
   def make_request(endpoint \\ "/", params \\ %{}) do
@@ -69,16 +68,18 @@ defmodule Mssum do
     access_token = Application.get_env(:app_vars, :githubAccessToken)
 
     case HTTPoison.get(
-      base_url,
-      ["Authorization": "token #{access_token}"],
-      params: params
-    ) do
+           base_url,
+           [Authorization: "token #{access_token}"],
+           params: params
+         ) do
       {:ok, %HTTPoison.Response{status_code: 200, body: body}} ->
         body
+
       {:ok, %HTTPoison.Response{status_code: 404}} ->
-        IO.puts "Not found :("
+        IO.puts("Not found :(")
+
       {:error, %HTTPoison.Error{reason: reason}} ->
-        IO.inspect reason
+        IO.inspect(reason)
     end
   end
 end
