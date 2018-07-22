1c6dda4c8fe76b0d113a327b7667ac21e4c8fa70
Yothin M <m@yothinix.com>
2018-07-22 11:28:52 +0700

    Read github token from config :rocket:

--- lib/mssum.ex
+++ lib/mssum.ex
@@ -1,14 +1,23 @@
 defmodule Mssum do
+  # require IEx; IEx.pry
   def main do
-    make_request()
+    user = "yothinix"
+    repository = "mssum"
+
+    make_request("/repos/#{user}/#{repository}/milestones") |> list_milestone
+
+  end
+
+  def list_milestone(milestones) do
+    Enum.map(milestones, fn milestone -> IO.puts milestone["url"] end)
   end
 
   def make_request(endpoint \\ "/") do
     base_url = "https://api.github.com#{endpoint}"
+    access_token = Application.get_env(:app_vars, :github_access_token)
 
-    case HTTPoison.get(base_url) do
+    case HTTPoison.get(base_url, ["Authorization": "token #{access_token}"]) do
       {:ok, %HTTPoison.Response{status_code: 200, body: body}} ->
-        IO.puts body
         body |> Poison.decode!
       {:ok, %HTTPoison.Response{status_code: 404}} ->
         IO.puts "Not found :("
